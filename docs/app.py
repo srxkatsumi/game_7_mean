a0e52b983dd88dd68547edca771ba3a8d14025bf
Vicky Costa <73000702+srxkatsumi@users.noreply.github.com>
2024-06-07 14:57:27 +0200

    Update app.py

--- docs/app.py
+++ docs/app.py
@@ -3,108 +3,108 @@ import random
 
 app = Flask(__name__)
 
-def valor_carta(carta):
-    if carta in ['Valete', 'Dama', 'Rei']:
+def card_value(card):
+    if card in ['Jack', 'Queen', 'King']:
         return 0.5
-    elif carta == 'Ás':
+    elif card == 'Ace':
         return 1
     else:
-        return int(carta)
+        return int(card)
 
-def sortear_carta(cartas):
-    carta = random.choice(cartas)
-    cartas.remove(carta)
-    return carta
+def draw_card(deck):
+    card = random.choice(deck)
+    deck.remove(card)
+    return card
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        fichas = int(request.form['fichas'])
-        aposta = int(request.form['aposta'])
+        chips = int(request.form['chips'])
+        bet = int(request.form['bet'])
 
-        if aposta < 50 or aposta > fichas:
+        if bet < 50 or bet > chips:
             return render_template_string('''
-                <h1>7 e Meio</h1>
-                <p>Aposta inválida. Tente novamente.</p>
+                <h1>7 and a Half</h1>
+                <p>Invalid bet. Please try again.</p>
                 <form method="post">
-                    <label>Fichas disponíveis: </label>
-                    <input type="number" name="fichas" value="{{ fichas }}" readonly><br>
-                    <label>Quanto deseja apostar (mínimo 50)?</label>
-                    <input type="number" name="aposta" required><br>
-                    <button type="submit">Jogar</button>
+                    <label>Available chips: </label>
+                    <input type="number" name="chips" value="{{ chips }}" readonly><br>
+                    <label>How much do you want to bet (minimum 50)?</label>
+                    <input type="number" name="bet" required><br>
+                    <button type="submit">Play</button>
                 </form>
-            ''', fichas=fichas)
+            ''', chips=chips)
 
-        cartas = ['Ás', '2', '3', '4', '5', '6', '7', 'Valete', 'Dama', 'Rei'] * 4
-        random.shuffle(cartas)
+        cards = ['Ace', '2', '3', '4', '5', '6', '7', 'Jack', 'Queen', 'King'] * 4
+        random.shuffle(cards)
 
-        mao_jogador = []
-        mao_banca = []
-        pontuacao_jogador = 0
-        pontuacao_banca = 0
+        player_hand = []
+        dealer_hand = []
+        player_score = 0
+        dealer_score = 0
 
-        carta_bonus = sortear_carta(cartas)
+        bonus_card = draw_card(cards)
 
         for _ in range(2):
-            carta = sortear_carta(cartas)
-            mao_banca.append(carta)
-            pontuacao_banca += valor_carta(carta)
+            card = draw_card(cards)
+            dealer_hand.append(card)
+            dealer_score += card_value(card)
 
-        carta = sortear_carta(cartas)
-        mao_jogador.append(carta)
-        pontuacao_jogador += valor_carta(carta)
+        card = draw_card(cards)
+        player_hand.append(card)
+        player_score += card_value(card)
 
-        if pontuacao_banca > 7.5:
-            fichas += aposta
-            mensagem = "A banca estourou 7.5, você venceu!"
-            premio = random.randint(50, 1000)
-            fichas += premio
-            mensagem += f" Você ganhou um prêmio de {premio} fichas!"
-        elif pontuacao_jogador == 7.5:
-            fichas += aposta
-            mensagem = "Você fez 7.5, você venceu!"
-            premio = random.randint(50, 1000)
-            fichas += premio
-            mensagem += f" Você ganhou um prêmio de {premio} fichas!"
-            if carta_bonus in mao_jogador:
-                bonus = aposta * 2
-                fichas += bonus
-                mensagem += f" Você tem a carta bônus {carta_bonus} e ganha um bônus de {bonus} fichas!"
+        if dealer_score > 7.5:
+            chips += bet
+            message = "The dealer exceeded 7.5, you win!"
+            prize = random.randint(50, 1000)
+            chips += prize
+            message += f" You won a prize of {prize} chips!"
+        elif player_score == 7.5:
+            chips += bet
+            message = "You got 7.5, you win!"
+            prize = random.randint(50, 1000)
+            chips += prize
+            message += f" You won a prize of {prize} chips!"
+            if bonus_card in player_hand:
+                bonus = bet * 2
+                chips += bonus
+                message += f" You have the bonus card {bonus_card} and win a bonus of {bonus} chips!"
         else:
-            if pontuacao_jogador < pontuacao_banca:
-                fichas -= aposta
-                mensagem = "Você perdeu! Continue tentando!"
+            if player_score < dealer_score:
+                chips -= bet
+                message = "You lose! Keep trying!"
             else:
-                fichas += aposta
-                mensagem = "Você venceu!"
-                premio = random.randint(50, 1000)
-                fichas += premio
-                mensagem += f" Você ganhou um prêmio de {premio} fichas!"
-                if carta_bonus in mao_jogador:
-                    bonus = aposta * 2
-                    fichas += bonus
-                    mensagem += f" Você tem a carta bônus {carta_bonus} e ganha um bônus de {bonus} fichas!"
+                chips += bet
+                message = "You win!"
+                prize = random.randint(50, 1000)
+                chips += prize
+                message += f" You won a prize of {prize} chips!"
+                if bonus_card in player_hand:
+                    bonus = bet * 2
+                    chips += bonus
+                    message += f" You have the bonus card {bonus_card} and win a bonus of {bonus} chips!"
 
         return render_template_string('''
-            <h1>7 e Meio</h1>
-            <p>{{ mensagem }}</p>
+            <h1>7 and a Half</h1>
+            <p>{{ message }}</p>
             <form method="post">
-                <label>Fichas disponíveis: </label>
-                <input type="number" name="fichas" value="{{ fichas }}" readonly><br>
-                <label>Quanto deseja apostar (mínimo 50)?</label>
-                <input type="number" name="aposta" required><br>
-                <button type="submit">Jogar</button>
+                <label>Available chips: </label>
+                <input type="number" name="chips" value="{{ chips }}" readonly><br>
+                <label>How much do you want to bet (minimum 50)?</label>
+                <input type="number" name="bet" required><br>
+                <button type="submit">Play</button>
             </form>
-        ''', mensagem=mensagem, fichas=fichas)
+        ''', message=message, chips=chips)
 
     return render_template_string('''
-        <h1>7 e Meio</h1>
+        <h1>7 and a Half</h1>
         <form method="post">
-            <label>Fichas disponíveis: </label>
-            <input type="number" name="fichas" value="300" readonly><br>
-            <label>Quanto deseja apostar (mínimo 50)?</label>
-            <input type="number" name="aposta" required><br>
-            <button type="submit">Jogar</button>
+            <label>Available chips: </label>
+            <input type="number" name="chips" value="300" readonly><br>
+            <label>How much do you want to bet (minimum 50)?</label>
+            <input type="number" name="bet" required><br>
+            <button type="submit">Play</button>
         </form>
     ''')
 
